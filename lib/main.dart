b8827cd00c59e086b94e16535e409fc3ec0b105a
unknown <aviggiano836@gmail.com>
2019-02-11 19:44:05 -0500

    Updated primary swatch to wic purple

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const MaterialColor purple = const MaterialColor(
+      0xFF4D1F5B,
+      const <int, Color>{
+        50: const Color(0xFF4D1F5B),
+        100: const Color(0xFF4D1F5B),
+        200: const Color(0xFF4D1F5B),
+        300: const Color(0xFF4D1F5B),
+        400: const Color(0xFF4D1F5B),
+        500: const Color(0xFF4D1F5B),
+        600: const Color(0xFF4D1F5B),
+        700: const Color(0xFF4D1F5B),
+        800: const Color(0xFF4D1F5B),
+        900: const Color(0xFF4D1F5B),
+      },
+    );
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'WiC Polls',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -18,7 +33,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: purple,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
