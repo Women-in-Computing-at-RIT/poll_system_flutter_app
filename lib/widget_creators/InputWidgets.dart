c4b01ffa51ffc34f16ece8e27ccefeb772198d49
unknown <aviggiano836@gmail.com>
2019-03-10 23:05:16 -0400

    Added skeleton code and comments to InputWidgets.dart

---
+++ lib/widget_creators/InputWidgets.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:poll_system_flutter_app/styles/InputThemes.dart';
+
+class InputWidgets{
+  InputThemes _inputThemes = new InputThemes();
+
+  /// Build a text input with the given placeholder string
+  TextInput buildTextInput(String placeholder){
+    //TODO
+    return null;
+  }
+
+  /// Build a checkbox with the given text
+  Checkbox buildCheckboxInput(String text){
+    //TODO
+    return null;
+  }
+
+  /// Build a list of checkboxes with the given list of options
+  Widget buildListOfCheckboxes(List<String> options){
+    //TODO
+    return null;
+  }
+
+  /// Build a radio button with the given text
+  Radio buildRadioButton (String text){
+    //TODO
+    return null;
+  }
+
+  /// Build a list of radio buttons with the list of options
+  Widget buildListOfRadios(List<String> options){
+    //TODO
+    return null;
+  }
+}
