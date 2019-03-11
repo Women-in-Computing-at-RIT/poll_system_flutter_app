7b5c6f7bc3ad98fa52728b73e57563ff76b88319
unknown <aviggiano836@gmail.com>
2019-03-10 22:42:34 -0400

    Added skeleton code to ColorScheme.dart

---
+++ lib/styles/ColorScheme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorScheme{
+  Color primaryColor; //TODO
+  Color secondaryColor; //TODO
+  Color accentColor; //TODO
+  ColorSwatch colorSwatch; //TODO
+}
