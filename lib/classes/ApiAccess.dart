4a223133c7df4e4fb3a0779a1b406216fda0b122
unknown <aviggiano836@gmail.com>
2019-03-10 21:03:27 -0400

    Added skeleton code for ApiAccess.dart

--- lib/classes/ApiAccess.dart
+++ lib/classes/ApiAccess.dart
@@ -1,7 +1,45 @@
+import 'package:poll_system_flutter_app/classes/Poll.dart';
+import 'package:poll_system_flutter_app/classes/User.dart';
+
 class ApiAccess {
   //TODO
 
   ApiAccess(){
     //TODO
   }
+
+  void createPoll(String name, bool canAddOption, bool multplieAllowed, List<String> options){
+    //TODO
+  }
+
+  User login(String username, String password){
+    //TODO
+    return null;
+  }
+
+  List<Poll> getAllCurrentPolls(){
+    //TODO
+    return null;
+  }
+
+  List<Poll> getAllPastPolls(){
+    //TODO
+    return null;
+  }
+
+  List<Poll> getAllPendingPolls(){
+    //TODO
+    return null;
+  }
+
+  List<Poll> getPollsForUser(String userid){
+    //TODO
+    return null;
+  }
+
+  void approvePoll(bool isApproved, int pollId){
+    //TODO
+  }
+
+
 }
