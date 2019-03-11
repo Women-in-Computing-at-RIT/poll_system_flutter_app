c27992770423babeb8458900d8318d03599cbfaf
unknown <aviggiano836@gmail.com>
2019-03-10 22:59:11 -0400

    Added skeleton code and comments to ButtonWidget.dart

---
+++ lib/widget_creators/ButtonWidgets.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:poll_system_flutter_app/styles/ButtonThemes.dart';
+
+class ButtonWidgets{
+  ButtonThemes _buttonThemes = new ButtonThemes();
+
+  /// Create a return a RaisedButton with the given text and function
+  ///   using the appropriate theme from ButtonThemes
+  RaisedButton buildRaisedButton(String text, Function func){
+    //TODO
+    return null;
+  }
+
+  /// Create a return a FlatButton with the given text and function
+  ///   using the appropriate theme from ButtonThemes
+  FlatButton buildFlatButton(String text, Function func){
+    //TODO
+    return null;
+  }
+
+  /// Create a return a IconButton with the given text and function
+  ///   using the appropriate theme from ButtonThemes
+  IconButton buildIconButton(String text, Function func){
+    //TODO
+    return null;
+  }
+
+  /// Create a return a ButtonBar with the given list of icon buttons
+  ///   using the appropriate theme from ButtonThemes
+  ButtonBar buildButtonBar(List<Widget> buttons){
+    //TODO
+    return null;
+  }
+}
