57c2bea078c2f7949acdcb3b47aa3256e8a39ec2
unknown <aviggiano836@gmail.com>
2019-03-10 22:50:17 -0400

    Added skeleton code to InputThemes.dart

---
+++ lib/styles/InputThemes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:poll_system_flutter_app/styles/PollColorScheme.dart';
+
+class InputThemes{
+  PollColorScheme _colorScheme = new PollColorScheme();
+
+  /// Return theme for a Text Input
+  Theme getTextInputTheme(){
+    //TODO
+    return null;
+  }
+}
