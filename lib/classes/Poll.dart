023f87f53055d6cfcee36fae25e694abef7e5af3
unknown <aviggiano836@gmail.com>
2019-03-10 22:34:56 -0400

    Updated Poll.dart class with skeleton code and comments

--- lib/classes/Poll.dart
+++ lib/classes/Poll.dart
@@ -1,27 +1,84 @@
+import 'dart:core';
+import 'dart:core';
+
+import 'package:poll_system_flutter_app/classes/PollOption.dart';
+
 class Poll {
   String _name; //poll name
-  String _owner; //owner of the poll
-  Map<String, int> _options;  //all the options of the poll
+  int _id; //poll id
+  int _owner; //owner of the poll
+  bool _canAddOptions;
+  bool _isMultipleSelect;
+
+  // map of the options for the poll with the poll id as a key
+  Map<int, PollOption> _options;
+
+  // Map of user ids and the option(s) they have voted for
+  Map<int, List<int>> _votes;
 
   Poll(String name, String owner, Map<String, int> options){
     //TODO
   }
 
-  /*
-   * Add a vote to the selected option
-   *    and send an update to the server
-   */
-  bool voteFor(String option){
+  /// *** Getters *** ///
+
+  /// Get name of poll
+  String getName(){
     //TODO
-    return false;
+    return null;
   }
 
-  /*
-   * Add option to the map
-   *    and request an update to the server
-   */
+  /// Get Map of Options
+  Map<int, PollOption> getOptions(){
+    //TODO
+    return null;
+  }
+
+  /// Get top option for poll
+  PollOption getTopOption(){
+    //TODO
+    return null;
+  }
+
+  /// Return if users other than owner can add options
+  bool canAddOption(){
+    //TODO
+    return null;
+  }
+
+  /// Return is users can vote for multiple options
+  bool isMultipleSelect(){
+    //TODO
+    return null;
+  }
+
+  /// Get votes for option with optId
+  int getVotesForOption(int optId){
+    //TODO
+    return null;
+  }
+
+  /// *** Updates *** ///
+
+  /// Update poll with given approval
+  void approvePoll(bool approval){
+    //TODO
+  }
+
+  /// Add a vote to the list of selected option(s)
+  void placeVote(List<int> options){
+    //TODO
+  }
+
+  /// Add vote to single option
+  void placedVote(int option){
+    //TODO
+  }
+
+  /// Check that user can add option
+  ///   If yes, add option to the map
   bool addOption(String option){
     //TODO
-    return false;
+    return null;
   }
 }
