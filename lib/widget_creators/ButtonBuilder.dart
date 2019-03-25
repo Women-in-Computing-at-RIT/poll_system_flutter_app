35e282d14c74b7b633132851da25f69764bafa3a
unknown <aviggiano836@gmail.com>
2019-03-25 18:32:24 -0400

    Create ButtonBuilder.dart skeleton code

---
+++ lib/widget_creators/ButtonBuilder.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:poll_system_flutter_app/styles/ButtonThemes.dart';
+import 'package:poll_system_flutter_app/widget_creators/ButtonWidgets.dart';
+
+class ButtonBuilder{
+    ButtonThemes _buttonThemes = new ButtonThemes();
+    ButtonWidgets _buttonWidgets = new ButtonWidgets();
+
+    /// Create and return Widget containing raised buttons
+    ///   created using the given map text and functions
+    ///   with the appropriate theme from ButtonThemes
+    Widget buildRaisedButtons(Map<String, Function> buttons){
+      //TODO
+      return null;
+    }
+
+    /// Create and return Widget containing flat buttons
+    ///   created using the given map text and functions
+    ///   with the appropriate theme from ButtonThemes
+    Widget buildFlatButtons(Map<String, Function> buttons){
+      //TODO
+      return null;
+    }
+
+    /// Create and return Widget containing icon buttons
+    ///   created using the given map text and functions
+    ///   with the appropriate theme from ButtonThemes
+    Widget buildIconButtons(Map<String, Function> buttons){
+      //TODO
+      return null;
+    }
+
+}
