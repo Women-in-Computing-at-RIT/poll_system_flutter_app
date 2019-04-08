6b3243954d244ba1158457e995296f90abe7a730
unknown <aviggiano836@gmail.com>
2019-04-08 18:57:45 -0400

    Finished TODOs for InputWidget functions

--- lib/widget_creators/InputWidgets.dart
+++ lib/widget_creators/InputWidgets.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:poll_system_flutter_app/styles/InputThemes.dart';
+import 'package:tuple/tuple.dart';
 
 class InputWidgets{
   InputThemes _inputThemes = new InputThemes();
@@ -18,26 +18,49 @@ class InputWidgets{
   }
 
   /// Build a checkbox with the given text
-  Checkbox buildCheckboxInput(String text){
-    //TODO
-    return null;
+  CheckboxListTile buildCheckboxInput(Tuple2<String, bool> checkbox){
+    bool isChecked = checkbox.item2;
+    return new CheckboxListTile(
+          value: isChecked,
+          onChanged: (value) {
+            //TODO?
+          },
+          title: Text(checkbox.item1),
+          controlAffinity: ListTileControlAffinity.leading,
+        );
   }
 
   /// Build a list of checkboxes with the given list of options
-  Widget buildListOfCheckboxes(List<String> options){
-    //TODO
-    return null;
+  Widget buildListOfCheckboxes(List<Tuple2<String, bool>> options){
+    var temp = <Widget>[];
+    options.forEach((value){
+        temp.add(buildCheckboxInput(value));
+    });
+
+    return new Column(
+      children: temp,
+    );
   }
 
   /// Build a radio button with the given text
-  Radio buildRadioButton (String text){
-    //TODO
-    return null;
+  RadioListTile buildRadioButton (Tuple2<String, bool> radio){
+    return new RadioListTile(
+        value: radio.item1,
+        groupValue: null,
+        onChanged: null,
+        title: Text(radio.item1),
+    );
   }
 
   /// Build a list of radio buttons with the list of options
-  Widget buildListOfRadios(List<String> options){
-    //TODO
-    return null;
+  Widget buildListOfRadios(List<Tuple2<String, bool>> options){
+    var temp = <Widget>[];
+    options.forEach((value){
+      temp.add(buildRadioButton(value));
+    });
+
+    return new Column(
+      children: temp,
+    );
   }
 }
