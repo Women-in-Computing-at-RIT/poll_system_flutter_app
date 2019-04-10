c7af3c9cc643cd8b09601bc797d5ce46570fb91e
dquach217 <dq6180@rit.edu>
2019-04-09 22:52:23 -0400

    push poll parser code

--- lib/classes/PollParser.dart
+++ lib/classes/PollParser.dart
@@ -11,88 +11,94 @@ class PollParser{
   Map<int, Poll> _userPolls;
 
   //User Id to get user polls
-  int _userid;
+  int _userId;
 
-  PollParser(int userid){
+  PollParser(int userId){
     _api = new ApiAccess();
-
-    //TODO init polls
+    _userId = userId;
   }
 
   /// Generic Function to parse objects from a List<Map<String, dynamic>
   ///   into a Poll and return as a Map<int, Poll>
   Map<int, Poll> _parsePolls(List<Map<String, dynamic>> rawPolls){
-    //TODO
-    return null;
+    return new Map.fromIterable(rawPolls, key: (v) => v[0], value: (v) => v[1]);
   }
 
   /// Call parser on raw current polls received from api
   ///   and add to map of current polls
   void _parseCurrentPolls(){
-    //TODO
+    _parsePolls(_api.getAllCurrentPolls());
   }
 
   /// Call parser on raw past polls received from api
   ///   and add to map of past polls
   void _parsePastPolls(){
-    //TODO
+    _parsePolls(_api.getAllPastPolls());
   }
 
 
   /// Call parser on raw pending polls received from api
   ///   and add to map of pending polls
   void _parsePendingPolls(){
-    //TODO
+    _parsePolls(_api.getAllPendingPolls());
   }
 
 
   /// Call parser on raw user polls received from api
   ///   and add to map of user polls
   void _parseUserPolls(){
-    //TODO
+    _parsePolls(_api.getPollsForUser(_userId));
   }
 
   /// Check that map of current polls has been initialized
   ///   If not call parser
   ///   If yes return map of current polls
   Map<int, Poll> getCurrentPolls(){
-    //TODO
-    return null;
+    if(_currentPolls == null) {
+      _parseCurrentPolls();
+    }
+    return _currentPolls;
   }
 
   /// Check that map of past polls has been initialized
   ///   If not call parser
   ///   If yes return map of past polls
   Map<int, Poll> getPastPolls(){
-    //TODO
-    return null;
+    if (_pastPolls == null) {
+      _parsePastPolls();
+    }
+    return _pastPolls;
   }
 
   /// Check that map of pending polls has been initialized
   ///   If not call parser
   ///   If yes return map of pending polls
   Map<int, Poll> getPendingPolls(){
-    //TODO
-    return null;
+    if (_pendingPolls == null) {
+      _parsePendingPolls();
+    }
+    return _pendingPolls;
   }
 
   /// Check that map of user's polls has been initialized
   ///   If not call parser
   ///   If yes return map of user's polls
   Map<int, Poll> getUserPolls(){
-    //TODO
-    return null;
+    if (_userPolls == null) {
+      _parseUserPolls();
+    }
+    return _userPolls;
   }
 
   /// Call the api to create a new pending poll
   void createPoll(String name, bool canAddOption, bool multplieAllowed, List<String> options){
-    //TODO
+    _api.createPoll(name, canAddOption, multplieAllowed, options);
   }
 
 
   /// Call apiAccess to update poll with given id
   ///   isApproved    poll is approved by admin
   void approvePoll(bool isApproved, int pollId){
-    //TODO
+    _api.approvePoll(isApproved, pollId);
   }
 }
