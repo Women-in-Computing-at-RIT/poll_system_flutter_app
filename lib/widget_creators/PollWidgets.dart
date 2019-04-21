c9272a6a942d832f687f2ddbfda78ee526992475
Kiersten Reina <kar5326@rit.edu>
2019-04-21 17:34:01 -0400

    finished build methods in pollWidgets

--- lib/widget_creators/PollWidgets.dart
+++ lib/widget_creators/PollWidgets.dart
@@ -2,20 +2,85 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:poll_system_flutter_app/classes/Poll.dart';
 import 'package:poll_system_flutter_app/classes/PollParser.dart';
+import 'package:poll_system_flutter_app/classes/User.dart';
+import 'package:poll_system_flutter_app/pages/CreatePollPage.dart';
 import 'package:poll_system_flutter_app/styles/PollColorScheme.dart';
+import 'package:poll_system_flutter_app/widget_creators/ButtonBuilder.dart';
+
+class PollExpansionPanel {
+  final WidgetBuilder bodyBuilder;
+  final String title;
+  final String subtitle;
+  bool isExpandedInitially;
+
+  PollExpansionPanel({
+    @required this.bodyBuilder,
+    @required this.title,
+    this.subtitle = "",
+    this.isExpandedInitially = false,
+  })  : assert(title != null),
+        assert(bodyBuilder != null);
+
+  ExpansionPanelHeaderBuilder get headerBuilder =>
+          (context, isExpanded) => new Row(children: [
+        new SizedBox(width: 100.0, child: new Text(title)),
+        new Text(subtitle)
+      ]);
+}
+
+class ExpansionList extends StatefulWidget {
+  /// The items that the expansion list should display; this can change
+  /// over the course of the object but probably shouldn't as it won't
+  /// transition nicely or anything like that.
+  final List<PollExpansionPanel> items;
+
+  ExpansionList(this.items) {
+    // quick check to make sure there's no duplicate titles.
+    assert(new Set.from(items.map((li) => li.title)).length == items.length);
+  }
+
+  @override
+  State<StatefulWidget> createState() => new ExpansionListState();
+}
+
+class ExpansionListState extends State<ExpansionList> {
+  Map<String, bool> expandedByTitle = new Map();
+
+  @override
+  Widget build(BuildContext context) {
+    return new ExpansionPanelList(
+      children: widget.items
+          .map(
+            (item) => new ExpansionPanel(
+            headerBuilder: item.headerBuilder,
+            body: new Builder(builder: item.bodyBuilder),
+            isExpanded:
+            expandedByTitle[item.title] ?? item.isExpandedInitially),
+      )
+          .toList(growable: false),
+      expansionCallback: (int index, bool isExpanded) {
+        setState(() {
+          expandedByTitle[widget.items[index].title] = !isExpanded;
+        });
+      },
+    );
+  }
+}
 
 class PollWidgets{
   PollParser _pollParser;
   PollColorScheme _colorScheme;
+  User _user;
 
   /// userid - to create PollParser
-  PollWidgets(int userid){
-    _pollParser = new PollParser(userid);
+  PollWidgets(User user){
+    _pollParser = new PollParser(user.getId());
     _colorScheme = new PollColorScheme();
   }
 
   /// Create a poll the user can vote on
   ExpansionPanel _buildVoteablePoll(Poll poll){
+    //TODO
     ExpansionPanel panel = new ExpansionPanel();
   }
 
@@ -34,50 +99,84 @@ class PollWidgets{
   }
 
   /// Create a view only poll with the options sorted based on votes
-  ExpansionPanel _buildViewOnlyPoll(Poll poll){
-    //TODO
-    return null;
+  PollExpansionPanel _buildViewOnlyPoll(Poll poll){
+    List<Widget> lst = new List();
+    poll.getSortedListOfOptions().forEach((value)=>
+        lst.add(Text(value.getText())));
+    return new PollExpansionPanel(bodyBuilder: (context) => Column(
+        children: lst,),
+        title: poll.getName(),
+        subtitle: poll.getTopOption().getText());
   }
 
   /// Build a widget containing all the past polls
   ///   Build each poll as a view only poll
   Widget buildPastPoll(){
-    ExpansionPanelList lst = new ExpansionPanelList();
+    List<PollExpansionPanel> lst = new List();
     Map<int, Poll>  polls = _pollParser.getPastPolls();
 
     if ( polls != null ) {
       polls.forEach((key, value) =>
-          lst.children.add(_buildViewOnlyPoll(value)));
+          lst.add(_buildViewOnlyPoll(value)));
     }
-
-    return lst;
+    return new ExpansionList(lst);
   }
 
   /// Build an editable poll
   ///   Editable has an option to edit and send user to new page to edit
-  ExpansionPanel _buildUserPoll(Poll poll){
-    //TODO
-    return null;
+  ListView _buildUserPoll(Poll poll){
+    return new ListView.builder(itemBuilder: (context, int) => Column(
+      children: <Widget>[Text(poll.getName()),
+        Text(poll.getTopOption().getText()),
+       ButtonBuilder().buildFlatButton("Edit", () {
+         Navigator.push(context, MaterialPageRoute(
+            builder: (context) =>
+            new CreatePollPage(
+                title: "Create Poll",
+                user: _user,
+                poll: poll
+            )
+         ));
+       }) ,
+      ]
+    ));
   }
 
   /// Build a widget containing all polls owned by the current user
   ///   Build each Poll as a UserPoll
   Widget buildUserPolls(){
-    ExpansionPanelList lst = new ExpansionPanelList();
     Map<int, Poll>  polls = _pollParser.getUserPolls();
-
+    List<Widget> lst = new List();
     if ( polls != null ) {
       polls.forEach((key, value) =>
-          lst.children.add(_buildUserPoll(value)));
+          lst.add(_buildUserPoll(value)));
     }
-
-    return lst;
+    ListView pollLst = new ListView.builder(itemBuilder: (context, int) =>
+      Column(
+        children: lst,
+      )
+    );
+    return pollLst;
   }
 
   /// Create a poll for an admin to approve or reject
-  ExpansionPanel _buildPendingPoll(Poll poll){
-    //TODO
-    return null;
+  PollExpansionPanel _buildPendingPoll(Poll poll){
+    List<Widget> lst = new List();
+    poll.getSortedListOfOptions().forEach((value)=>
+        lst.add(Text(value.getText())));
+    lst.add(Row(
+        children: <Widget>[
+          ButtonBuilder().buildFlatButton("Approve", () {
+          _pollParser.approvePoll(true, poll.getId());
+        }),
+          ButtonBuilder().buildFlatButton("Decline", () {
+          _pollParser.approvePoll(false, poll.getId());
+        })
+        ]));
+    return new PollExpansionPanel(bodyBuilder: (context) => Column(
+        children: lst,),
+        title: poll.getName(),
+        subtitle: null);
   }
 
   /// Build a widget containing all polls pending approval
