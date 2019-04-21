23350963eff4eb18ba75c9afff62ce75ee4f721b
dquach217 <dq6180@rit.edu>
2019-04-21 15:23:21 -0400

    update createPoll with description and pubspec.yaml

--- lib/classes/ApiAccess.dart
+++ lib/classes/ApiAccess.dart
@@ -1,28 +1,36 @@
-import 'package:http/http.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+
+import 'package:http/http.dart' as http;
 
 class ApiAccess {
   //TODO
   String path;
   var client; //Using clients to access the api
 
-  ApiAccess(){
+  ApiAccess() {
     //TODO
-    client = new Client();
+    client = new http.Client();
   }
 
+  final String url = "wic_polls.serveo.net/";
+
   /// Call the api to insert a new pending poll
-  void createPoll(String name, bool canAddOption, bool multplieAllowed, List<String> options){
-    //TODO
+  void createPoll(String name, String description, bool canAddOption,
+      bool multiplesAllowed, List<String> options) {
   }
 
   /// Call api to attempt a login
   ///   return first Map from resulting List<Map<String, dynamic>>
-  Map<String, dynamic> login(String username, String password){
+  Map<String, dynamic> login(String username, String password) {
     //TODO
     var user = Map<String, dynamic>();
     user['username'] = username;
     user['id'] = 5;
-    if(username == 'admin'){
+    if (username == 'admin') {
       user['isAdmin'] = true;
     } else {
       user['isAdmin'] = false;
@@ -32,36 +40,33 @@ class ApiAccess {
 
   /// Call api to get all approved polls
   ///   with a lifespan of less than 2 weeks since approval
-  List<Map<String, dynamic>> getAllCurrentPolls(){
+  List<Map<String, dynamic>> getAllCurrentPolls() {
     //TODO
     return null;
   }
 
   /// Call api to get all approved polls
   ///   with a lifespan of more than 2 weeks since approval
-  List<Map<String, dynamic>> getAllPastPolls(){
+  List<Map<String, dynamic>> getAllPastPolls() {
     //TODO
     return null;
   }
 
   /// Call api to get all pending polls
-  List<Map<String, dynamic>> getAllPendingPolls(){
+  List<Map<String, dynamic>> getAllPendingPolls() {
     //TODO
     return null;
   }
 
   /// Call api to get all current polls
   ///   owned by the user with the given id
-  List<Map<String, dynamic>> getPollsForUser(int userid){
+  List<Map<String, dynamic>> getPollsForUser(int userid) {
     //TODO
     return null;
   }
 
   /// Call api to update poll with given id
   ///   isApproved    poll is approved by admin
-  void approvePoll(bool isApproved, int pollId){
-    //TODO
+  void approvePoll(bool isApproved, int pollId) {
   }
-
-
 }
